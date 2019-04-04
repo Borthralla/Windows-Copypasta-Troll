1d1702423f12c76ffd01e6111c05ddecdfe68d4d
Philip Mann <pmann@decibeltx.com>
2019-04-04 15:30:25 -0400

    Use pythonw for true sneaky

--- create_task.cmd
+++ create_task.cmd
@@ -1,1 +1,1 @@
-SCHTASKS /Create /TN "copypasta" /TR "%userprofile%\AppData\Local\Programs\Python\Python37\python.exe %cd%\copypasta.pyw" /SC MINUTE /MO 1
+SCHTASKS /Create /TN "copypasta" /TR "%userprofile%\AppData\Local\Programs\Python\Python37\pythonw.exe %cd%\copypasta.pyw" /SC MINUTE /MO 1
